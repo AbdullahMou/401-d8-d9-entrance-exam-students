c102183bc514cb99e7a534b4a4d9b02460d9c8b3
AbdullahMouzeen <abdullah.mouazeen@gmail.com>
2020-11-18 12:27:07 +0200

    done

---
+++ data/schema.sql
@@ -0,0 +1,7 @@
+create table characters (
+    id serial primary key,
+    name varchar(255),
+    patronus  varchar(255),
+    alive varchar(255),
+    image varchar(255)
+);
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict'
+//'use strict'
 // -------------------------
 // Application Dependencies
 // -------------------------
@@ -43,18 +43,85 @@ app.set('view engine', 'ejs');
 // ----------------------
 // ------- Routes -------
 // ----------------------
+app.get('/home', handleHome);
+
+app.get('/characters', handlecharacters);
+app.post('/my-characters', handlefav);
+app.get('/my-characters', handlefav1);
+app.get('/my-characters/:id', handledetails);
+app.put('/my-characters/:id', handleupdate);
 
 
 // --------------------------------
 // ---- Pages Routes functions ----
 // --------------------------------
+function handleHome(req, res) {
 
+    res.render('index');
+}
+//-----------------------------------------------------------
+function handlecharacters(req, res) {
+    let url = HP_API_URL;
+    superagent.get(url).then(data => {
 
+        let hpArr = data.body.map(val => {
+                return new Character(val)
+            })
+            //console.log(hpArr);
+        res.render('characters', { result: hpArr })
 
+    })
+}
+//-----------------------------------------------------------
+
+function Character(val) {
+    this.name = val.name ? val.name : 'no name';
+    this.patronus = val.patronus ? val.patronus : 'no patronus';
+    this.alive = val.alive;
+    this.image = val.image;
+}
 // -----------------------------------
 // --- CRUD Pages Routes functions ---
 // -----------------------------------
+//-----------------------------------------------------------
+function handlefav(req, res) {
+    let { name, image, alive, patronus } = req.body;
 
+    let = sql = `insert into characters (name,image,alive,patronus) values ($1,$2,$3,$4);`;
+    let val = [name, image, alive, patronus];
+    client.query(sql, val).then(data => {
+        //console.log(data);
+        res.redirect('/my-characters')
+    })
+}
+//-----------------------------------------------------------
+
+function handlefav1(req, res) {
+    let sql = `select * from characters ;`;
+    client.query(sql).then(data => {
+        res.render('my-characters', { result: data.rows })
+    })
+}
+//-------------------------------------------------------------------
+function handledetails(req, res) {
+    let sql = `select * from characters where id=$1;`;
+    let val = [req.params.id];
+    client.query(sql, val).then(data => {
+        console.log(data.rows);
+        res.render('details', { result: data.rows })
+    })
+}
+//-------------------------------------------------------------------
+
+function handleupdate(req, res) {
+    let { name, image, alive, patronus } = req.body;
+    let sql = `update characters set name=$1, image=$2, alive=$3, patronus=$4 where id=$5 ;`;
+    let val = [name, image, alive, patronus, req.params.id];
+    client.query(sql, val).then(data => {
+        console.log(data);
+        res.redirect(`/details/${req.params.id}`)
+    })
+}
 
 
 // Express Runtime
